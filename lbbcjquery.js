5d302e530f664fb29a244078a8dd8d0d07526cff
Diane <diane.douglas1@gmail.com>
2017-01-24 23:32:15 -0500

    Append links with custom class for styling at the end of the main page content.

--- lbbcjquery.js
+++ lbbcjquery.js
@@ -1,14 +1,22 @@
-var allLinks = [];
 jQuery(document).ready(function($){
+  var allLinks = [];
   $('.node-blog-stories__field-secondary-topics-item a').each(function(index){
-    var currentLink = $(this).attr('href');
+    var currentHref = $(this).attr('href');
     var currentText = $(this).text();
     allLinks[index] = { 
       currentText: currentText,
-      currentLink: currentLink
+      currentHref: currentHref
     };
   });
   console.log(allLinks);
+  // $("<p>Test</p>").insertAfter('.node-blog-stories--full .content');
+  
+  // add plain anchor tag for each link after the main content div of the page. Give it a class for styling of "blog-stories-tag"
+  for (var i = 0; i < allLinks.length; i++) {
+    var currentLink = allLinks[i];
+    $('<a class="blog-stories-tag" href="' + currentLink.currentHref + '">' + currentLink.currentText + '</a>').insertAfter('.node-blog-stories--full .content');
+  };
+  
+  // at the end clear out the variable.
   allLinks = [];
-  $("<p>Test</p>").insertAfter('.node-blog-stories--full .content');
 });

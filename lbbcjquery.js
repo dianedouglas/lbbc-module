606794af69499c7033397681edd9da7ff65c3d41
Diane <diane.douglas1@gmail.com>
2017-01-24 23:26:10 -0500

    structure links into an object and insert test paragraph after .content div inside of .node-blog-stories--full div.

--- lbbcjquery.js
+++ lbbcjquery.js
@@ -1,7 +1,14 @@
+var allLinks = [];
 jQuery(document).ready(function($){
-  $('.node-blog-stories__field-secondary-topics-item a').each(function(){
+  $('.node-blog-stories__field-secondary-topics-item a').each(function(index){
     var currentLink = $(this).attr('href');
     var currentText = $(this).text();
-    console.log(currentText);
+    allLinks[index] = { 
+      currentText: currentText,
+      currentLink: currentLink
+    };
   });
+  console.log(allLinks);
+  allLinks = [];
+  $("<p>Test</p>").insertAfter('.node-blog-stories--full .content');
 });
